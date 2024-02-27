0586215074104579d421a99b9c1fa71796960efb
Guillermo <guillermofabrod@gmial.com>
2024-02-27 16:50:15 +0100

    esta casi pero algo falla

--- ts/Controladores/ListadoController.ts
+++ ts/Controladores/ListadoController.ts
@@ -1,40 +1,55 @@
-// Cuando el contenido del DOM está completamente cargado
 document.addEventListener('DOMContentLoaded', function() {
-  // Llamar a la función para cargar los Digimon
-  CargarDigimon(0);
-  // Las siguientes funciones están comentadas. Pueden ser llamadas más adelante si son necesarias.
-  // CargarAtributo();
-  // CargarFields();
-  // CargarLevels();
-  // CargarTipos();
-  // Mostrar la página después de cargar los Digimon
-  mostrarpagina();
+  let digimondcardslist: Card[] = [];
+  let pagina = obtenerParametroDeURL("pagina");
+
+    CargarDigimon(pagina);
+    const digimoncardsString = localStorage.getItem('digimoncards');
+    
+    if (digimoncardsString !== null) {
+      digimondcardslist = JSON.parse(digimoncardsString);
+    
+      digimondcardslist.forEach((digimon: Card) => {
+      console.log(pagina);
+      pintarlista(digimon);
+    });
+  }
 });
 
-// Función para mostrar la página
-function mostrarpagina() {
-  // Obtener el array de Digimon del localStorage
-  const digimonarray = JSON.parse(localStorage.getItem('digimoncards') || '[]');
-  // Iterar sobre cada Digimon en el array y pintarlo en la lista
-  digimonarray.forEach((digimon : Card) => {
-      pintarlista(digimon);
-  });
+
+function pintarlista(digimon: Card) {
+  const listado = $("#lista");
+  let copia = $("#cartica").clone(true, true);
+  copia.removeAttr('id');
+  copia.find('.card-title').text(digimon.nombre);
+  copia.find('.card-img-top').attr('src', digimon.imagen);
+  copia.find('.enlace-digimon').attr('href', "Datos.html?id=" + digimon.id);
+  copia.appendTo(listado).show();
 }
 
-// Función para pintar un Digimon en la lista
-function pintarlista(digimon: Card) {
-  // Seleccionar el contenedor de la lista
-  const listado = $("#lista");
-  // Clonar el div #cartica
-  let copia = $("#cartica").clone(true, true);
-  // Eliminar el atributo id de la copia para evitar duplicados en los identificadores
-  copia.removeAttr('id');
-  // Cambiar el texto de la tarjeta con el nombre del Digimon
-  copia.find('.card-title').text(digimon.nombre);
-  // Cambiar la imagen de la tarjeta con la imagen del Digimon
-  copia.find('.card-img-top').attr('src', digimon.imagen);
-  // Establecer el enlace de la tarjeta para redirigir a la página de detalles del Digimon
-  copia.find('.enlace-digimon').attr('href', "Datos.html?id="+digimon.id); 
-  // Agregar la copia al contenedor de la lista y mostrarla
-  copia.appendTo(listado).show();
+function pagAtras() {
+  const paginaActual = obtenerParametroDeURL("pagina");
+  if (paginaActual > 0) {
+    irA((paginaActual - 1).toString());
+  }
+}
+
+function pagAdelante() {
+  const paginaActual = obtenerParametroDeURL("pagina");
+  if (paginaActual < 49) {
+    irA((paginaActual + 1).toString());
+  }
+}
+
+function obtenerParametroDeURL(pagina: string): number {
+  const urlActual = window.location.href;
+  const parametrosURL = new URLSearchParams(new URL(urlActual).search);
+  let valorParametro = parametrosURL.get(pagina);
+  if (valorParametro === null) {
+    valorParametro = "0";
+  }
+  return parseInt(valorParametro);
+}
+
+function irA(pagina: string) {
+  window.location.href = "Listado.html?pagina=" + pagina;
 }
